3b5e35a88515f3d8e414c890b1c58048a6f139f2
dj <davidabraham@ucla.edu>
2020-08-18 06:39:40 -0700

    added nevilles

--- julia/modules/interpolate.jl
+++ julia/modules/interpolate.jl
@@ -1,7 +1,18 @@
 module Interpolate
 
-function nevilles_method(f::Function, x::Array)
+function nevilles_method(f::Function, x::Real, xarr::Array)
+    n = size(xarr)
+    q = zeros(n, n)
 
+    for i in 1:n
+        for j in 1:i
+            num = (x - xarr[i-j]) * q[i,j-1] - (x - xarr[i]) * q[i-1,j-1]
+            dem = x - xarr[i-j]
+            q[i,j] = num / dem
+        end
+    end
+
+    return q
 end
 
 
