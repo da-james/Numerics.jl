1436b8ecc555b9717856854edd14054ff6f67424
dj <davidabraham@ucla.edu>
2020-08-18 19:28:00 -0700

    changed indexing

--- julia/modules/interpolate.jl
+++ julia/modules/interpolate.jl
@@ -3,12 +3,13 @@ module Interpolate
 function nevilles_method(f::Function, x::Real, xarr::Array)
     n = size(xarr)
     q = zeros(n, n)
+    q[:,1] = f(xarr)
 
     for i in 1:n
         for j in 1:i
-            num = (x - xarr[i-j]) * q[i,j-1] - (x - xarr[i]) * q[i-1,j-1]
-            dem = x - xarr[i-j]
-            q[i,j] = num / dem
+            num = (x - xarr[i-j+1]) * q[i+1,j+1] - (x - xarr[i+1]) * q[i,j]
+            dem = x - xarr[i-j+1]
+            q[i+1,j+1] = num / dem
         end
     end
 
