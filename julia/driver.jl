c23388845ad41aae871bedaa826e80a50975a8e3
dj <davidabraham@ucla.edu>
2020-08-22 12:59:58 -0700

    added newton's difference

--- julia/driver.jl
+++ julia/driver.jl
@@ -7,14 +7,18 @@ using .Interpolate
 
 function main()
 
-    f(x) = x - (x^3 + 4*x^2 - 10) / (3*x^2 + 8*x)
+    # f(x) = x - (x^3 + 4*x^2 - 10) / (3*x^2 + 8*x)
     # zero = Root.bisection_method(f, -2, -3)
     # sol = Root.fixed_point(f, 1.5)
     # println(sol)
 
-    x = 1940:10:1990
-    q = [132165, 151326, 179323, 203302, 226542, 249633]
-    println(Interpolate.nevilles_method(2000, 5, x, q))
+    # x = 1940:10:1990
+    # q = [132165, 151326, 179323, 203302, 226542, 249633]
+    # println(Interpolate.nevilles_method(2000, 5, x, q))
+
+    x = [8.1, 8.3, 8.6, 8.7]
+    y = [16.9446, 17.56492, 18.50515, 18.82091]
+    println(Interpolate.newtons_difference(x, y))
 
 end
 
--- julia/modules/interpolate.jl
+++ julia/modules/interpolate.jl
@@ -13,5 +13,20 @@ function nevilles_method(x0::Real, n::Int64, x::AbstractArray, q::Array)
     return q[1]
 end
 
+function newtons_difference(x::AbstractArray, q::AbstractArray)
+
+    n = size(x)[1]
+    f = zeros(n, n)
+    f[:,1] = q
+
+    for i in 2:n
+        for j in i:n
+            f[j, i] = (f[j,i-1] - f[j-1,i-1]) / (x[j] - x[j-i+1])
+        end
+    end
+
+    return f
+end
+
 
 end # end of module
