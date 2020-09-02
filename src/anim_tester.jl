e0195a863cacbef126862bae8b17cd14b717dcdb
dj <davidabraham@ucla.edu>
2020-09-02 13:50:50 -0700

    different animation

--- src/anim_tester.jl
+++ src/anim_tester.jl
@@ -1,5 +1,20 @@
-ENV["PYCALL_JL_RUNTIME_PYTHON"] = "/home/dajames/Documents/projects/venvProjectsP3/bin/python3"
-using PyCall
+using Plots
 
-using PyPlot
-@pyimport matplotlib.animation as anim
+# anim = Animation()
+# p = plot([sin,cos], 0, π, size=(300,300))
+# scatter!([0], [sin,cos])
+# for i in 0:0.1:π
+#     p[3] = [i], [sin(i)]
+#     p[4] = [i], [cos(i)]
+#     frame(anim)
+# end
+# gif(anim, "anim_fps15.gif", fps = 15)
+
+p = plot([sin, cos], zeros(0), leg = false)
+anim = Animation()
+for x = range(0, stop = 10π, length = 100)
+    push!(p, x, Float64[sin(x), cos(x)])
+    frame(anim)
+end
+
+gif(anim, "anim_fps30.gif", fps = 30)
