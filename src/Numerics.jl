b9d47c384564109c671231a118f864d290063f4b
David James <davidabraham@ucla.edu>
2021-12-14 06:56:29 -0800

    fixing exports

--- src/Numerics.jl
+++ src/Numerics.jl
@@ -8,4 +8,23 @@ include("ode.jl")
 include("pde.jl")
 include("roots.jl")
 
+using Approximate, BVP, Interpolate, LinAlg, NDE, ODE, PDE, Roots
+
+export
+    finite_difference,
+    nevilles_method,
+    gauss_elimination,
+    crout_factorization,
+    gauss_sidel_method,
+    norm,
+    jacobian,
+    newtons_system,
+    rk4_method,
+    rk4_system,
+    trapezoid_method,
+    biscetion_method,
+    fixed_point,
+    newtons_method,
+    secant_method
+
 end # module
