f1f9cf46c3b9427f796bce34f9417a5372f2afd4
David <davidabraham@ucla.edu>
2025-05-13 13:22:58 -0700

    updated derivs function to handle functions better

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,7 +1,7 @@
 module Conversions
 
 export bar_to_Pa, GPa_to_Pa, kbar_to_GPa, kbar_to_MPa, A_to_Bohr
-export eV_to_J, Ry_to_eV, A_to_m, Gyr_to_sec
+export eV_to_J, Ry_to_eV, A_to_m, yr_to_sec, Gyr_to_sec
 
 const bar_to_Pa = 1e5
 const GPa_to_Pa = 1e9
@@ -11,6 +11,7 @@ const eV_to_J = 1.602176565e-19
 const Ry_to_eV = 13.605662285137
 const A_to_m = 1e-10
 const A_to_Bohr = 1.889644746787604
-const Gyr_to_sec = 1e9 * 365 * 24 * 60 * 60
+const yr_to_sec = 365 * 24 * 60 * 60
+const Gyr_to_sec = 1e9 * yr_to_sec
 
 end # module
--- src/derivative.jl
+++ src/derivative.jl
@@ -2,14 +2,14 @@ module Derivative
 
 export deriv
 
-function deriv(f, x0::Real, p, h::Real=0.001)
+function deriv(f, x0::Real, args...; h::Real=1e-3)
 
-    k1 = f(x0 - 2h, p...)
-    k2 = f(x0 - h, p...)
-    k3 = f(x0 + h, p...)
-    k4 = f(x0 + 2h, p...)
+    k1 = f(x0 - 2h, args...)
+    k2 = f(x0 - h, args...)
+    k3 = f(x0 + h, args...)
+    k4 = f(x0 + 2h, args...)
 
-    return 1/(12*h) * (k1 - 8*k2 + 8*k3 - k4)
+    return (k1 - 8*k2 + 8*k3 - k4) / (12*h)
 end
 
 end # module
