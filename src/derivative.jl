281314912fce7f8811454a902423a68124dd0c47
David <davidabraham@ucla.edu>
2025-05-26 15:14:40 -0700

    added a parameter for deriv to make it more stable for step and a ln_deriv; not tested

--- src/derivative.jl
+++ src/derivative.jl
@@ -2,14 +2,15 @@ module Derivative
 
 export deriv
 
-function deriv(f, x0::Real, args...; h::Real=1e-3)
+function deriv(f, x0::Real, args...; h::Real=1e-3, rel::Real=1e-3)
 
-    k1 = f(x0 - 2h, args...)
-    k2 = f(x0 - h, args...)
-    k3 = f(x0 + h, args...)
-    k4 = f(x0 + 2h, args...)
+    h₀ = max(h, rel*abs(x0))
+    k1 = f(x0 - 2h₀, args...)
+    k2 = f(x0 - h₀, args...)
+    k3 = f(x0 + h₀, args...)
+    k4 = f(x0 + 2h₀, args...)
 
-    return (k1 - 8*k2 + 8*k3 - k4) / (12*h)
+    return (k1 - 8*k2 + 8*k3 - k4) / (12*h₀)
 end
 
 end # module
